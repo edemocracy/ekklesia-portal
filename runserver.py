ceb29d66f42468d37673849b63718582f4c9508d
dpausp <dpausp@posteo.de>
2015-09-07 08:59:49 +0200

    i18n
    * replaced all message strings with gettext calls
    * added flask-babelex
    * makebabel.ipy script
    * translations in de an en

--- runserver.py
+++ runserver.py
@@ -1,4 +1,5 @@
+import os.path
 from arguments import make_app
 
 flask_app = make_app()
-flask_app.run(host="0.0.0.0", port=5000, debug=True)
+flask_app.run(host="0.0.0.0", port=5000, debug=True, extra_files=[os.path.join(os.path.dirname(__file__), ".babelcompiled")])
