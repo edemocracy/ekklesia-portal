95c373d6a15e2a3502f5b72820b608fa2bfaf797
Tobias 'escaP <escaP@piraten-oberpfalz.de>
2015-09-11 17:42:18 +0200

    config from config.py, oauth2 with ekkesia basics
    * app config is loaded from config.py, if it exists
    * oauth2 communication with ekklesia server works
        * flask-oauthlib default client seems to not support refresh token
        * change to requests-oauthlib later or contrib client from flask-oauthlib

--- arguments/views/__init__.py
+++ arguments/views/__init__.py
@@ -2,3 +2,5 @@ from .questions import questions
 from .question import question
 from .argument import argument
 from .counter_arguments import counter_arguments
+from .oauth import oauth_callback, login
+
