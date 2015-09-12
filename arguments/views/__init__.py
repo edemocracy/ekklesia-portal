8edfb035927136c9d33e8c3cebcd5f0b201ef493
Tobias 'escaP <escaP@piraten-oberpfalz.de>
2015-09-12 11:54:35 +0200

    adding new arguments works
    * author is faked, replace later

--- arguments/views/__init__.py
+++ arguments/views/__init__.py
@@ -1,6 +1,6 @@
 from .questions import questions
 from .question import question
-from .argument import argument
+from .argument import argument, new_argument
 from .counter_arguments import counter_arguments
 from .oauth import oauth_callback, login
 
