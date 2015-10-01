54d735b95d30e21254cc04fbf76f5a60db066b92
Tobias 'escaP <escaP@piraten-oberpfalz.de>
2015-10-01 15:19:17 +0000

    exclude unwanted columns in user admin view

--- arguments/views/admin/__init__.py
+++ arguments/views/admin/__init__.py
@@ -10,9 +10,16 @@ class BaseAdminView(ModelView):
         super(BaseAdminView, self).__init__(model, session, *args, **kwargs)
 
 
+class QuestionView(ModelView):
+    column_exclude_list = ("details", "search_vector")
+
+    def __init__(self, session=db.session, *args, **kwargs):
+        super(QuestionView, self).__init__(Question, session, category="Content", *args, **kwargs)
+
+
 admin.add_view(BaseAdminView(User, category="User"))
 admin.add_view(BaseAdminView(UserGroup, category="User"))
-admin.add_view(BaseAdminView(Question, category="Content"))
+admin.add_view(QuestionView())
 admin.add_view(BaseAdminView(Argument, category="Content"))
 admin.add_view(BaseAdminView(Tag, category="Content"))
 admin.add_view(BaseAdminView(QuestionVote, category="Vote"))
