03fc545f4bfb20d259cc1eb2532f78e4ca3b8a94
dpausp <dpausp@posteo.de>
2015-11-27 16:16:47 +0000

    change import style for views, test view
    * we don't need to import every single view function explicitly ;)

--- arguments/views/__init__.py
+++ arguments/views/__init__.py
@@ -1,5 +1,23 @@
-from .questions import questions
-from .question import question
-from .argument import argument, new_argument
-from .counter_arguments import counter_arguments
-from .logout import logout
+"""
+All view modules must be imported here.
+"""
+
+import logging
+from flask import render_template
+from arguments import app
+
+
+logg = logging.getLogger(__name__)
+
+
+@app.route("/test")
+def test():
+    return render_template("test.j2.jade")
+
+
+# import all view functions defined is this package to register them with the app
+from . import question
+from . import questions
+from . import argument
+from . import counter_arguments
+from . import logout
