af580517e7f29fa1c23ff29d8016a3efce8d692d
dpausp <dpausp@posteo.de>
2017-11-14 18:40:49 +0000

    use current nixpkgs-unstable

--- requirements/nixpkgs.nix
+++ requirements/nixpkgs.nix
@@ -1,1 +1,7 @@
-fetchTarball https://nixos.org/channels/nixos-16.09/nixexprs.tar.xz
+let 
+  fetchNixpkgs = import ./fetchNixpkgs.nix;
+  nixpkgs = fetchNixpkgs {
+    rev = "eac38d0b1e68b4ae4f0b78fe778b61d0f314ae7a";
+    sha256 = "00fcaga6802dkc5ikcfm3vwbqpaspqi5j626lmnmpqi6zc5x9ibc";
+  };
+in nixpkgs
