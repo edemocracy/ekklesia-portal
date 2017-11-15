cc3edd74acc5979828e3876e6b4f4a9b4bb51f83
dpausp <dpausp@posteo.de>
2017-11-15 18:04:22 +0000

    morepath.autoscan needs deps in setup.py, load them from Pipfile
    ... installing the package seems to be neccessary, too.

--- requirements/update-nix-requirements.sh
+++ requirements/update-nix-requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # lock packages and creates a requirements file with pinned versions and hashes
 pipenv lock -r > pypi2nix_source_deps_with_hash
-# strip hashes
-awk '{ print $1 }' pypi2nix_source_deps_with_hash > pypi2nix_source_deps
+# strip hashes and filter out self-dependency -e . 
+awk '{ if ($1!="-e") print $1 }' pypi2nix_source_deps_with_hash | grep -v '\-e .' > pypi2nix_source_deps
 # create requirements.nix from intermediary pypi2nix_source_deps
 pypi2nix -V 3.6 -r pypi2nix_source_deps -E postgresql -E libffi
--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
-# see setup.cfg for metadata and options
+# see setup.cfg for metadata and options; only requirements are loaded here
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+
 from setuptools import setup
-setup()
+setup(install_requires=requirements)
