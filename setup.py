37d758b1209746feaa974593c8c9578be734646d
dpausp <dpausp@posteo.de>
2017-11-15 08:49:14 +0000

    setup.py: version and reqs from text files; add test deps

--- setup.py
+++ setup.py
@@ -5,34 +5,25 @@ import re
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Read the version number from a source file.
-# Why read it, and not import?
-# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
-def find_version(*file_paths):
-    # Open in Latin-1 so that we avoid encoding errors.
-    # Use codecs.open for Python 2 compatibility
-    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
-        version_file = f.read()
 
-    # The version line must have the form
-    # __version__ = 'ver'
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+def find_version():
+    with open(os.path.join(here, "arguments", "VERSION")) as version_file:
+        return version_file.read().strip()
+
+
+def read_requirements_file(requirements_type):
+    with open(os.path.join(here, "requirements", requirements_type + "_requirements.txt")) as f:
+        return [s.strip() for s in f.readlines()]
 
 
 setup(
     name="arguments",
-    version=find_version('arguments', '__init__.py'),
+    version=find_version(),
     description="arguments",
     # The project URL.
     url="",
-    # Author details
     author='dpausp',
     author_email='dpausp@posteo.de',
-    # Choose your license
     license='GNU General Public License v3 (GPLv3)',
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
@@ -41,11 +32,13 @@ setup(
         'Topic :: Database'
     ],
     keywords='arguments ekklesia evoting edemocracy democracy',
-    packages=["arguments",
-              "arguments.views",
-              "arguments.views.admin",
-              ],
-    install_requires=[],
-    setup_requires=["setuptools-git"],
+    packages=find_packages(),
+    install_requires=read_requirements_file("install"),
+    setup_requires=read_requirements_file("setup"),
+    tests_require=read_requirements_file("test"),
+    extras_require = {
+        "devel": read_requirements_file("devel")
+    },
+    python_requires="~=3.6",
     include_package_data=True,
 )
