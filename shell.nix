0db2ea6ea13ed704d8deeb6f421b06aacdbf3bea
dpausp <dpausp@posteo.de>
2017-12-12 10:47:37 +0000

    run zsh pipenv shell automatically in nix shell

--- shell.nix
+++ shell.nix
@@ -5,5 +5,7 @@ in with pkgs; stdenv.mkDerivation {
   src = null;
   name = "arguments-dev-env";
   phases = [];
-  buildInputs = with python36Packages; [ sassc ipython ipdb pyflakes pipenv ];
+  buildInputs = with python36Packages; [ sassc ipython ipdb pyflakes pipenv zsh ];
+  shellHook = "SHELL=`which zsh` pipenv shell --fancy";
 }
+
