ad58333fb283c84d78d4bafe8ca8a3482f7c6ac4
dpausp <dpausp@posteo.de>
2017-12-12 18:02:50 +0000

    fix nix-build's problem with version read from file in setup.cfg
    Has worked before, don't know why it fails now...
    Read the version in setup.py instead.

--- setup.py
+++ setup.py
@@ -5,10 +5,15 @@ import os.path
 here = os.path.abspath(os.path.dirname(__file__))
 
 
+def find_version():
+    with open(os.path.join(here, "arguments", "VERSION")) as version_file:
+        return version_file.read().strip()
+
+
 def read_install_requires():
     with open(os.path.join(here, "requirements", "install_requirements.txt")) as f:
         return [s.strip() for s in f.readlines()]
 
 
 from setuptools import setup
-setup(install_requires=read_install_requires())
+setup(install_requires=read_install_requires(), version=find_version())
