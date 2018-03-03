9d2680556c92005d466ebf050167612650473dda
dpausp <dpausp@posteo.de>
2018-03-03 21:08:49 +0000

    update nixpkgs, use fetchFromGithub for fetching nixpkgs

--- requirements/nixpkgs.nix
+++ requirements/nixpkgs.nix
@@ -1,7 +1,9 @@
 let 
-  fetchNixpkgs = import ./fetchNixpkgs.nix;
-  nixpkgs = fetchNixpkgs {
-    rev = "eac38d0b1e68b4ae4f0b78fe778b61d0f314ae7a";
-    sha256 = "00fcaga6802dkc5ikcfm3vwbqpaspqi5j626lmnmpqi6zc5x9ibc";
+  _systemNixpkgs = import <nixpkgs> {};
+  nixpkgs = _systemNixpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "6326053490c70114656c0f04c0c894a249a7eab0";
+    sha256 = "1z1v01k75s54r4vl2dsrxpfmdyn09j9lk4w1cp941mskyglmrg49";
   };
 in import nixpkgs {}
