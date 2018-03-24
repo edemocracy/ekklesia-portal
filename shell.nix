361853c8f18b41612bd3e24651488b9881c04869
dpausp <dpausp@posteo.de>
2018-03-24 13:21:59 +0000

    ipdb / ipython have readline problems, remove them from shell.nix for now
    -   installing ipdb via pipenv works better

--- shell.nix
+++ shell.nix
@@ -5,6 +5,6 @@ in with pkgs; stdenv.mkDerivation {
   src = null;
   name = "ekklesia_portal-dev-env";
   phases = [];
-  buildInputs = with python36Packages; [ sassc ipython ipdb pyflakes pipenvCustom zsh ];
+  buildInputs = with python36Packages; [ sassc pyflakes pipenvCustom zsh ];
   #shellHook = "PYTHONPATH= SHELL=`which zsh` pipenv shell --fancy";
 }
