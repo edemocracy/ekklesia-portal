d2df3a3973b5864349e79b9ebf0a7b562e97252b
dpausp <dpausp@posteo.de>
2018-06-04 16:49:35 +0000

    update nixpkgs and pipenv

--- pipenv.nix
+++ pipenv.nix
@@ -3,10 +3,10 @@
 pipenv.overrideAttrs(oldAttrs: rec {
   inherit (oldAttrs) pname;
   name = "${pname}-${version}";
-  version = "11.10.1";
+  version = "2018.5.18";
 
   src = python36Packages.fetchPypi {
     inherit pname version;
-    sha256 = "1jw25l66j7g68wq7pdg460y9ij868li12li57pn3avm3nvad9kws";
+    sha256 = "1knyknmykjj7gixdpfyns77sv4mizl68addk09ajmw9z5aqaif84";
   };
 })
--- requirements/nixpkgs.nix
+++ requirements/nixpkgs.nix
@@ -3,7 +3,7 @@ let
   nixpkgs = _systemNixpkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "6326053490c70114656c0f04c0c894a249a7eab0";
-    sha256 = "1z1v01k75s54r4vl2dsrxpfmdyn09j9lk4w1cp941mskyglmrg49";
+    rev = "7295e175bf6c6e8aa54f1b4d99256ee95d13d385";
+    sha256 = "1h5d2nlyh4w4i51gihj7vs14pg8vaam3ks93250xv7brx978cy66";
   };
 in import nixpkgs {}
