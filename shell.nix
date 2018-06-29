6efa2de78ad0a752ac7ca0b5851eccdda162fcb3
dpausp <dpausp@posteo.de>
2018-06-29 16:25:10 +0000

    Postgres 10 for Nix dev env

--- shell.nix
+++ shell.nix
@@ -5,6 +5,6 @@ in with pkgs; stdenv.mkDerivation {
   src = null;
   name = "ekklesia_portal-dev-env";
   phases = [];
-  buildInputs = with python36Packages; [ sassc pyflakes pipenvCustom zsh ];
+  buildInputs = with python36Packages; [ sassc pyflakes pipenvCustom zsh postgresql100 ];
   #shellHook = "PYTHONPATH= SHELL=`which zsh` pipenv shell --fancy";
 }
