519c9042d9135bba7cb7bb60c8a78deb5132d089
dpausp <dpausp@posteo.de>
2018-08-03 10:52:27 +0000

    fix update-nix-requirements script, use Python 3.7
    -   Python 3.7 needs a local checkout of nixpkgs python-unstable at $HOME/git/nixpkgs
    -   pypi2nix must be used with unstable

--- requirements/update-nix-requirements.sh
+++ requirements/update-nix-requirements.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 pushd requirements
 # lock packages and create requirements files with pinned versions and hashes
-pipenv lock -r | sort > all_deps # contains all dependencies
-pipenv lock -r -d | sort > dev_deps # contains only deps from [dev-packages]
-
-# only use deps which are in all_deps, but not in dev_deps
-comm -23 all_deps dev_deps > pypi2nix_source_deps_with_hash
+pipenv lock -r | sort > pypi2nix_source_deps_with_hash # contains all dependencies (no dev deps)
 
 # strip hashes and filter out self-dependency -e . 
 awk '{ if ($1!="-e") print $1 }' pypi2nix_source_deps_with_hash | grep -v '\-e .' > pypi2nix_source_deps
 # create requirements.nix from intermediary pypi2nix_source_deps
-pypi2nix -V 3.6 -r pypi2nix_source_deps -E postgresql -E libffi
+pypi2nix -I nixpkgs=$HOME/git/nixpkgs -V 3.7 -r pypi2nix_source_deps -E postgresql -E libffi
 popd
