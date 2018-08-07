30f0c066a3dfe36ca121e849aba81945e3416ef3
dpausp <dpausp@posteo.de>
2018-08-07 20:35:17 +0000

    test login special cases

--- tests/concepts/ekklesia_portal/test_login.py
+++ tests/concepts/ekklesia_portal/test_login.py
@@ -1,3 +1,8 @@
+from munch import Munch
+from pytest import raises
+from ekklesia_portal.concepts.ekklesia_portal.login import Login, UserNotFound
+
+
 def test_show_login(client):
     res = client.get("/login")
     content = res.body.decode()
@@ -21,3 +26,27 @@ def test_submit_login_wrong_password(client):
 
 def test_submit_login_incomplete(client):
     client.post("/login", dict(username="testuser"), status=400)
+
+
+def test_find_user_with_wrong_username_raises_user_not_found(req):
+    login = Login(request=req, username='invalid', password='')
+    with raises(UserNotFound):
+        login.find_user()
+
+
+def test_verify_password_without_user_raises_value_error():
+    login = Login()
+    with raises(ValueError):
+        login.verify_password(False)
+
+
+def test_verify_password_is_false_when_user_pw_is_none():
+    login = Login()
+    login.user = Munch(password=None)
+    assert login.verify_password(False) == False
+
+
+def test_verify_password_is_true_for_empty_pw_with_insecure_mode():
+    login = Login(username='test', password='')
+    login.user = Munch(password='pw')
+    assert login.verify_password(True) == True
