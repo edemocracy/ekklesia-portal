f0a9513d51005257dff6081c7a741a896d7fbb38
dpausp <dpausp@posteo.de>
2018-08-08 19:41:19 +0000

    add name arg assert_(no_)difference for better failure output
    It may be hard to tell which assert is which.  They support names now which are included in the assertion failure output.

--- tests/helpers/assert_helpers.py
+++ tests/helpers/assert_helpers.py
@@ -2,16 +2,23 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def assert_no_difference(func):
+def assert_no_difference(func, name=None):
     """Asserts that the function result is unchanged after executing the block"""
     val = func()
     yield
     val_after = func()
-    assert val_after == val, f'Result is not the same! Value before {val}, after: {val_after}'
+
+    def assertion_msg():
+        msg = f'result is not the same! value before {val}, after: {val_after}'
+        if name:
+            return f'{name}: ' + msg
+        return msg
+
+    assert val_after == val, assertion_msg()
 
 
 @contextmanager
-def assert_difference(func, diff):
+def assert_difference(func, diff, name=None):
     """
     Asserts that the function result has changed by `diff` after executing the block
     `diff` can be anything, but addition with the result of `func` must be defined.
@@ -19,4 +26,11 @@ def assert_difference(func, diff):
     val = func()
     yield
     val_after = func()
-    assert val_after == val + diff, f'Result did not change by {diff}! Value before {val}, after: {val_after}'
+
+    def assertion_msg():
+        msg = f'result for did not change by {diff}! value before {val}, after: {val_after}'
+        if name:
+            return f'{name}: ' + msg
+        return msg
+
+    assert val_after == val + diff, assertion_msg()
