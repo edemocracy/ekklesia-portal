c0b86431b5aa7bfde1e5807804f478e9d97c396a
dpausp <dpausp@posteo.de>
2018-09-07 22:30:25 +0000

    enhanced development start page
    -   links for creating new propositions
    -   show if insecure dev mode is enabled

--- src/ekklesia_portal/concepts/ekklesia_portal/cell/index.py
+++ src/ekklesia_portal/concepts/ekklesia_portal/cell/index.py
@@ -4,5 +4,8 @@ from ekklesia_portal.concepts.proposition.propositions import Propositions
 
 class IndexCell(LayoutCell):
 
-    def proposition_url(self):
-        return self.link(Propositions())
+    def insecure_development_mode_enabled(self):
+        return self._app.settings.app.insecure_development_mode
+
+    def new_proposition_url(self):
+        return self.link(Propositions(), '+new')
