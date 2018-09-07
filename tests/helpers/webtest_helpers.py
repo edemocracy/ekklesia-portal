166cd596463658a6bad72c1605e5ebd1dfdd94dd
dpausp <dpausp@posteo.de>
2018-09-07 22:32:27 +0000

    webtest helpers: improve assert_deform, add fill_form
    -   assert_deform can compare bool vals and other stuff that has the same string representation in python as in deform
    -   assert_deform shows which assertion failed
    -   fill_form fills deform fields

--- tests/helpers/webtest_helpers.py
+++ tests/helpers/webtest_helpers.py
@@ -1,12 +1,38 @@
-def assert_deform(response, expected_form_data):
+def python_to_deform_value(py_value):
+    if py_value == True:
+        return 'true'
+    elif py_value == False:
+        return 'false'
+    else:
+        return str(py_value)
+
+
+def assert_deform(response, expected_form_data={}):
     assert 'deform' in response.forms
     form = response.forms['deform']
 
     for field, value in expected_form_data.items():
-        assert form[field].value == value
+        if field == 'id':
+            continue
+
+        form_value = form[field].value
+        assert form_value == python_to_deform_value(value), f'form field {field}: form value {form_value} != {value}'
+
+    return form
 
 
 def get_session(app, client):
     serializer = app.browser_session_interface.get_signing_serializer(app)
     assert 'session' in client.cookies
     return serializer.loads(client.cookies['session'])
+
+
+def fill_form(form, data, field_names=None):
+    if field_names is None:
+        for field_name, value in data.items():
+            form.set(field_name, value)
+    else:
+        for field_name in field_names:
+            form.set(field_name, data[field_name])
+
+    return form
