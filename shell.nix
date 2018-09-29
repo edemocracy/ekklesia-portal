57405838abc04ba3f885629f5e7ab91599667f6b
dpausp <dpausp@posteo.de>
2018-09-29 13:02:29 +0000

    remove pyflakes from nix deps

--- shell.nix
+++ shell.nix
@@ -4,6 +4,6 @@ in pkgs.stdenv.mkDerivation {
   src = null;
   name = "ekklesia_portal-dev-env";
   phases = [];
-  buildInputs = with pkgs.python37Packages; [ pkgs.sassc pyflakes pkgs.pipenv pkgs.zsh pkgs.postgresql100 python  ];
+  buildInputs = with pkgs.python37Packages; [ pkgs.sassc pkgs.pipenv pkgs.zsh pkgs.postgresql100 python ];
   #shellHook = "PYTHONPATH= SHELL=`which zsh` pipenv shell --fancy";
 }
