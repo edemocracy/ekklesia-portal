d5953c81f8287ffcce3cb33d08d8fbc327a7c8c6
dpausp <dpausp@posteo.de>
2018-10-03 18:25:27 +0000

    FormCell handles form setup now, add NewFormCell and EditFormCell
    -   FormCell calls _prepare_form_for_render before rendering the form to HTML
        -   can be used for setting up widgets depending on information from the model or request
    -   NewFormCell and EditFormCell: base classes for standard new / edit views

--- src/ekklesia_portal/concepts/ekklesia_portal/cell/form.py
+++ src/ekklesia_portal/concepts/ekklesia_portal/cell/form.py
@@ -3,10 +3,31 @@ from .layout import LayoutCell
 
 class FormCell(LayoutCell):
 
-    def __init__(self, form, request, form_data, collection=None, layout=None, parent=None, template_path=None, **options):
-        self.form = form
-        self.form_data = form_data or {}
-        super().__init__(form, request, collection, layout, parent, template_path, **options)
+    def __init__(self, form, request, form_data=None, model=None, layout=None, parent=None, template_path=None, **options):
+        self._form = form
+        self._form_data = form_data or {}
+        super().__init__(model, request, None, layout, parent, template_path, **options)
+
+    def _prepare_form_for_render(self):
+        pass
 
     def form_html(self):
-        return self.__class__.markup_class(self.form.render(self.form_data))
+        self._prepare_form_for_render()
+        html = self._form.render(self._form_data)
+        return self.__class__.markup_class(html)
+
+
+class NewFormCell(FormCell):
+
+    def __init__(self, request, form, form_data=None, layout=None, parent=None, template_path=None, **options):
+        super().__init__(form, request, form_data, layout=layout, parent=parent, template_path=template_path, **options)
+
+
+class EditFormCell(FormCell):
+
+    def _prepare_form_for_render(self):
+        self._form_data = self._model.to_dict()
+
+    def __init__(self, model, request, form, layout=None, parent=None, template_path=None, **options):
+        super().__init__(form, request, model=model, layout=layout, parent=parent, template_path=template_path, **options)
+
