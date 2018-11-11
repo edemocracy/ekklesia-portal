69bfb4cc20570909f56af4150daefc908775982b
dpausp <dpausp@posteo.de>
2018-11-11 11:12:51 +0000

    fix update-nix-requirements for newer Pipenv versions
    The output of pipenv lock -r has changed which makes thing easier for
    the script.
    
    It still doesn't work at the moment because pypi2nix has a problem with
    wheel requirements parsing.

--- requirements/update-nix-requirements.sh
+++ requirements/update-nix-requirements.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 pushd requirements
-# lock packages and create requirements files with pinned versions and hashes
-pipenv lock -r | sort > pypi2nix_source_deps_with_hash # contains all dependencies (no dev deps)
-
-# strip hashes and filter out self-dependency -e . 
-awk '{ if ($1!="-e") print $1 }' pypi2nix_source_deps_with_hash | grep -v '\-e .' > pypi2nix_source_deps
+# lock packages and create requirements files with pinned versions
+pipenv lock -r > pypi2nix_source_deps # contains all dependencies (no dev deps)
 # create requirements.nix from intermediary pypi2nix_source_deps
 pypi2nix -I nixpkgs=$HOME/git/nixpkgs -V 3.7 -r pypi2nix_source_deps -E postgresql -E libffi
 popd
