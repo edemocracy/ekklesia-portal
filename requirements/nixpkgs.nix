40e3de72de9f8f7b5069231abccdbaeae537dd15
dpausp <dpausp@posteo.de>
2019-03-20 13:48:02 +0000

    update nixpkgs, use pipenv from nixpkgs

--- requirements/nixpkgs.nix
+++ requirements/nixpkgs.nix
@@ -3,7 +3,7 @@ let
   nixpkgs = _systemNixpkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "bea4b361a0e3a21b1e9a69d56f1ebd16fb38a296";
-    sha256 = "0ybslsfvib4lrq6vj5mibmpjvwcd5xljjr2zp23zmihcjvc4d1r9";
+    rev = "dc7bddbb8927258644d90e914abd96776ef83a5b";
+    sha256 = "0w67gcy7wpy6bihm0pkd4h2gr71g7kppsh0vkw71g7nn51i2yz5k";
   };
 in import nixpkgs {}
--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 { usePipenvShell ? true }:
 let
   pkgs = import ./requirements/nixpkgs.nix;
-  pipenv = pkgs.callPackage ./requirements/pipenv.nix { inherit pkgs; };
+  # pipenv = pkgs.callPackage ./requirements/pipenv.nix { inherit pkgs; };
 in pkgs.stdenv.mkDerivation {
   src = null;
   name = "ekklesia_portal-dev-env";
   phases = [];
-  buildInputs = with pkgs.python37Packages; [ pkgs.sassc pipenv pkgs.zsh pkgs.postgresql100 python ];
+  buildInputs = with pkgs.python37Packages; [ pkgs.sassc pkgs.pipenv pkgs.zsh pkgs.postgresql_11 python ];
   shellHook = if usePipenvShell then "PYTHONPATH= SHELL=`which zsh` exec pipenv shell --fancy" else "export PYTHONPATH=";
 }
