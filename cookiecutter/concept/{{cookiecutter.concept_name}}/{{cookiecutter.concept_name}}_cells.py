49a1f7230f37bd2f8f441c365478f9f04d099d34
dpausp <dpausp@posteo.de>
2019-03-23 23:57:52 +0000

    concept cookiecutter: adapt to form cell changes, improve cell documentation

--- cookiecutter/concept/{{cookiecutter.concept_name}}/{{cookiecutter.concept_name}}_cells.py
+++ cookiecutter/concept/{{cookiecutter.concept_name}}/{{cookiecutter.concept_name}}_cells.py
@@ -12,33 +12,47 @@ class {{ cookiecutter.ConceptNames }}Cell(LayoutCell):
     def {{ cookiecutter.concept_names }}(self):
         return list(self._model.{{ cookiecutter.concept_names }}(self._request.q))
 
-    # a method that can be used in the template (works without call parentheses)
+    # Methods from this class can be called from the template. 
+
+    # Methods with only the self argument can be used without call parentheses and their result value is cached.
     def show_new_button(self):
         return self.options.get('show_new_button') and self._request.permitted_for_current_user(self._model, CreatePermission)
 
 
 class {{ cookiecutter.ConceptName }}Cell(LayoutCell):
     model = {{ cookiecutter.ConceptName }}
-    #model_properties = ['name']
+
+    # Model attributes included here are available as variables in the template: 
+    # = name
+    # model_properties = ['name']
 
     def show_edit_button(self):
         return self.options.get('show_edit_button') and self._request.permitted_for_current_user(self._model, EditPermission)
 
 
 class New{{ cookiecutter.ConceptName }}Cell(NewFormCell):
+    pass
 
-    def _prepare_form_for_render(self):
-        pass
-        # for select fields
-        #items = items_for_{{cookiecutter.concept_name}}_select_widgets(self._model)
-        #self._form.prepare_for_render(items)
+    # def _prepare_form_for_render(self):
+        # By default, the form's prepare_for_render() method is called without arguments.
+        # You can pass additional args, for example to set values for select fields like this:
+
+        # items = items_for_{{cookiecutter.concept_name}}_select_widgets(self._model)
+        # self._form.prepare_for_render(items)
 
 
 class Edit{{ cookiecutter.ConceptName }}Cell(EditFormCell):
+    pass
 
-    def _prepare_form_for_render(self):
-        form_data = self._model.to_dict()
-        self.set_form_data(form_data)
-        # for select fields
-        #items = items_for_{{cookiecutter.concept_name}}_select_widgets(self._model)
-        #self._form.prepare_for_render(items)
+    # def _prepare_form_for_render(self):
+        # By default, all fields from the model as given by to_dict() are passed to the form.
+        # You can customize the behaviour (inherited from the base class) here:
+
+        # form_data = self._model.to_dict()
+        # self.set_form_data(form_data)
+
+        # By default, the form's prepare_for_render() method is called without arguments.
+        # You can pass additional args, for example to set values for select fields like this:
+
+        # items = items_for_{{cookiecutter.concept_name}}_select_widgets(self._model)
+        # self._form.prepare_for_render(items)
