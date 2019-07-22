d7d0a1fa72d2b54dc76a5e29cf26af0fa4bc500e
dpausp <dpausp@posteo.de>
2019-07-22 20:16:50 +0000

    cleanup shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
 { usePipenvShell ? true }:
 let
   pkgs = import ./requirements/nixpkgs.nix;
-  # pipenv = pkgs.callPackage ./requirements/pipenv.nix { inherit pkgs; };
 in pkgs.stdenv.mkDerivation {
   src = null;
   name = "ekklesia_portal-dev-env";
   phases = [];
-  buildInputs = with pkgs.python37Packages; [ pkgs.sassc pkgs.pipenv pkgs.zsh pkgs.postgresql_11 python ];
+  buildInputs = with pkgs; [ 
+    pipenv
+    postgresql_11
+    python37Packages.python 
+    sassc
+    zsh
+  ];
   shellHook = if usePipenvShell then "PYTHONPATH= SHELL=`which zsh` exec pipenv shell --fancy" else "export PYTHONPATH=";
 }
