c775b159b310afd323945afcb9dba771731a382b
dpausp <dpausp@posteo.de>
2019-10-25 07:19:29 +0000

    use repr for log serialization if json fails

--- src/ekklesia_portal/__init__.py
+++ src/ekklesia_portal/__init__.py
@@ -2,14 +2,24 @@ import eliot
 import logging
 import sys
 from eliot.stdlib import EliotHandler
+from eliot.json import EliotJSONEncoder
+
+class MyEncoder(EliotJSONEncoder):
+    def default(self, obj):
+
+        try:
+            return EliotJSONEncoder.default(self, obj)
+        except TypeError:
+            return repr(obj)
+
 
 logging.getLogger().addHandler(EliotHandler())
 logging.getLogger().setLevel(logging.DEBUG)
-eliot.to_file(sys.stdout)
+
+eliot.to_file(sys.stdout, encoder=MyEncoder)
+
 logging.captureWarnings(True)
 
 logg = logging.getLogger(__name__)
 
 logging.getLogger("parso").setLevel(logging.WARN)
-
-logg.info("init")
