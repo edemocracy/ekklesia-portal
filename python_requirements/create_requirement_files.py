63c777fc44077028b737541e3a8f28ce0c63a933
dpausp <dpausp@posteo.de>
2019-11-04 11:05:09 +0000

    Fix requirement scripts
    * missing popd
    * no need to print the setup.cfg...

--- python_requirements/create_requirement_files.py
+++ python_requirements/create_requirement_files.py
@@ -2,7 +2,7 @@ from pprint import pprint
 from setuptools.config import read_configuration
 conf = read_configuration('../setup.cfg')
 options = conf['options']
-pprint(conf)
+
 
 with open('install_requirements.txt', 'w') as wf:
     wf.write('\n'.join(options['install_requires']))
--- update_requirements.sh
+++ update_requirements.sh
@@ -2,11 +2,13 @@
 
 pushd python_requirements
 python create_requirement_files.py
+popd
+
 pushd nix
 
 pypi2nix -V python37 -r ../python_requirements/install_requirements.txt \
   --basename install_requirements \
-  -E postgresql -E libffi -E openssl.dev
+  -E postgresql_11 -E libffi -E openssl.dev
 
 pypi2nix -V python37 -r ../python_requirements/test_requirements.txt \
   --basename test_requirements
