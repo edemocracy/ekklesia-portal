52ed31eaf3929a1ec1860e39bb30a64cdb047c0a
Nico Borgsmüller <kaenganxt@mc-anura.de>
2020-06-09 23:41:14 +0200

    Search improvements (#56)
    - Support quoted words for filters
    - Ignore case in filters

--- src/ekklesia_portal/concepts/proposition/propositions.py
+++ src/ekklesia_portal/concepts/proposition/propositions.py
@@ -1,7 +1,7 @@
 import dataclasses
 from dataclasses import dataclass
-from ekklesia_portal.database.datamodel import Proposition, Ballot, VotingPhase, PropositionStatus, PropositionType, SubjectArea, Department
-from sqlalchemy import desc
+from ekklesia_portal.database.datamodel import Proposition, Ballot, VotingPhase, PropositionStatus, PropositionType, SubjectArea, Department, Tag
+from sqlalchemy import desc, func
 from sqlalchemy_searchable import search
 
 
@@ -32,7 +32,7 @@ class Propositions:
         self.type = self.type or None
 
         try:
-            self.status = PropositionStatus(self.status)
+            self.status = PropositionStatus(self.status.lower() if self.status else None)
         except ValueError:
             self.status = None
 
@@ -44,27 +44,68 @@ class Propositions:
             return
 
         search = []
-        for word in self.search.split():
-            if ":" in word:
-                (type, term) = word.split(':', 1)
-                if type == "status":
-                    self.status = term
-                elif type == "tag":
-                    self.tag = term
-                elif type == "phase":
-                    self.phase = term
-                elif type == "type":
-                    self.type = term
-                elif type == "department":
-                    self.department = term
-                elif type == "subject_area":
-                    self.subject_area = term
+
+        key = None
+        word = []
+        quot_started = None
+        word_done = False
+        for c in self.search.strip():
+            if c == '"' or c == '\'':
+                if quot_started == c:  # If at quoted string end
+                    word_done = True
+                    quot_started = None
+                elif not quot_started and len(word) == 0:  # If at quoted string start
+                    quot_started = c
+                else:  # If quote in middle of word
+                    word.append(c)
+
+            elif c == ':':
+                if quot_started:  # Colon in quoted string belongs to word
+                    word.append(c)
+                elif len(word) > 0:  # Only allow colons after text
+                    key = ''.join(word)
+                    word.clear()
+
+            elif c == ' ':
+                if quot_started:  # Whitespace in quoted string belongs to word
+                    word.append(c)
                 else:
-                    search.append(word)
+                    word_done = True
             else:
-                search.append(word)
+                word.append(c)
+
+            if word_done:
+                word_done = False
+                value = ''.join(word)
+                word.clear()
+                if key:
+                    self.parse_search_filter(key, value)
+                    key = None
+                else:
+                    search.append(value)  # Quoted strings in search currently have no special meaning
+
+        if len(word) > 0:
+            value = ''.join(word)
+            if key:
+                self.parse_search_filter(key, value)
+            else:
+                search.append(value)  # Quoted strings in search currently have no special meaning
 
         self.search = " ".join(search)
+
+    def parse_search_filter(self, key, value):
+        if key == "status":
+            self.status = value
+        elif key == "tag":
+            self.tag = value
+        elif key == "phase":
+            self.phase = value
+        elif key == "type":
+            self.type = value
+        elif key == "department":
+            self.department = value
+        elif key == "subject_area":
+            self.subject_area = value
 
     def build_search_query(self):
         query = []
@@ -73,19 +114,29 @@ class Propositions:
                 query.append(word)
 
         if self.status:
-            query.append("status:" + self.status)
+            query.append("status:" + self.maybe_add_quotes(self.status))
         if self.tag:
-            query.append("tag:" + self.tag)
+            query.append("tag:" + self.maybe_add_quotes(self.tag))
         if self.phase:
-            query.append("phase:" + self.phase)
+            query.append("phase:" + self.maybe_add_quotes(self.phase))
         if self.type:
-            query.append("type:" + self.type)
+            query.append("type:" + self.maybe_add_quotes(self.type))
         if self.department:
-            query.append("department:" + self.department)
+            query.append("department:" + self.maybe_add_quotes(self.department))
         if self.subject_area:
-            query.append("subject_area:" + self.subject_area)
+            query.append("subject_area:" + self.maybe_add_quotes(self.subject_area))
 
         return " ".join(query)
+
+    @staticmethod
+    def maybe_add_quotes(value):
+        if " " in value or ":" in value or '"' in value or "'" in value:
+            if '"' in value:
+                value = f"'{value}'"
+            else:
+                value = f'"{value}"'
+
+        return value
 
     def propositions(self, q):
         propositions = q(Proposition)
@@ -99,24 +150,24 @@ class Propositions:
             propositions = propositions.filter_by(status=self.status)
 
         if self.tag:
-            propositions = propositions.join(*Proposition.tags.attr).filter_by(name=self.tag)
+            propositions = propositions.join(*Proposition.tags.attr).filter(func.lower(Tag.name) == func.lower(self.tag))
 
         # Filters based on ballot
         if self.phase or self.type or self.department:
             propositions = propositions.join(Ballot)
 
             if self.phase:
-                propositions = propositions.join(VotingPhase).filter_by(name=self.phase)
+                propositions = propositions.join(VotingPhase).filter(func.lower(VotingPhase.name) == func.lower(self.phase))
 
             if self.type:
-                propositions = propositions.join(PropositionType).filter_by(abbreviation=self.type)
+                propositions = propositions.join(PropositionType).filter(func.lower(PropositionType.abbreviation) == func.lower(self.type))
 
             if self.department:
                 propositions = propositions.join(SubjectArea)
                 if self.subject_area:
-                    propositions = propositions.filter_by(name=self.subject_area)
+                    propositions = propositions.filter(func.lower(SubjectArea.name) == func.lower(self.subject_area))
 
-                propositions = propositions.join(Department).filter_by(name=self.department)
+                propositions = propositions.join(Department).filter(func.lower(Department.name) == func.lower(self.department))
 
         # Order
         if self.sort == "supporter_count":
