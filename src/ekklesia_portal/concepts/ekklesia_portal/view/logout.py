c602995e3f574ea1ae7536314b76c7423b3074f1
Tobias dpausp <dpausp@posteo.de>
2020-09-03 22:26:04 +0000

    Properly log out from OIDC provider

--- src/ekklesia_portal/concepts/ekklesia_portal/view/logout.py
+++ src/ekklesia_portal/concepts/ekklesia_portal/view/logout.py
@@ -1,6 +1,7 @@
 from morepath import redirect
 
 from ekklesia_portal.app import App
+from .index import Index
 
 
 @App.path(path='/logout')
@@ -15,4 +16,13 @@ def logout(self, request):
     def forget(response):
         request.app.forget_identity(response, request)
 
-    return redirect('/')
+    index_url = request.link(Index())
+
+    if request.ekklesia_auth.authorized:
+        # Redirect to the logout endpoint of the OIDC provider which then redirects back to our index page.
+        logout_url = request.ekklesia_auth.logout_url(index_url)
+    else:
+        # Not logged in to an OIDC provider, just go back to start.
+        logout_url = index_url
+
+    return redirect(logout_url)
