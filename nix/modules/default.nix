be8257cfc7720c5a368046838fd6c6f2f0788773
Tobias dpausp <dpausp@posteo.de>
2021-03-15 19:43:58 +0000

    NixOS module: secrets, hardening, various improvements

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -5,17 +5,14 @@ with builtins;
 let
   cfg = config.services.ekklesia.portal;
 
-  defaultConfig = {
+  configFilename = "ekklesia-portal-config.json";
 
-  };
-
-  appConfigFile =
-    pkgs.writeText "ekklesia-portal-config.json"
-      (toJSON
-        (lib.recursiveUpdate defaultConfig cfg.extraConfig));
+  configInput =
+    pkgs.writeText configFilename
+      (toJSON cfg.extraConfig);
 
   serveApp = import ../serve_app.nix {
-    inherit appConfigFile;
+    appConfigFile = "/run/ekklesia-portal/${configFilename}";
     listen = "${cfg.address}:${toString cfg.port}";
     tmpdir = "/tmp";
   };
@@ -34,6 +31,15 @@ in {
   options.services.ekklesia.portal = with lib; {
 
     enable = mkEnableOption "Enable the portal component of the Ekklesia e-democracy platform";
+
+    debug = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        (UNSAFE) Activate debugging mode for this module.
+        Currently shows how secrets are replaced in the pre-start script.
+      '';
+    };
 
     user = mkOption {
       type = types.str;
@@ -59,6 +65,31 @@ in {
       description = "Address for gunicorn app server";
     };
 
+    configFile = mkOption {
+      internal = true;
+      type = with types; nullOr path;
+      default = null;
+    };
+
+    browserSessionSecretKeyFile = mkOption {
+      type = types.str;
+      description = "Path to file containing the secret key for browser session signing";
+      default = "/var/lib/ekklesia-portal/browser-session-secret-key";
+    };
+
+    secretFiles = mkOption {
+      type = types.attrs;
+      default = {};
+      description = ''
+        Arbitrary secrets that should be read from a file and
+        inserted in the config on startup. Expects an attrset with
+        the variable name to replace and a file path to the secret.
+      '';
+      example = {
+        some_secret_api_key = "/var/lib/ekklesia-portal/some-secret-api-key";
+      };
+    };
+
     extraConfig = mkOption {
       type = types.attrs;
       default = {};
@@ -69,6 +100,8 @@ in {
 
   config = lib.mkIf cfg.enable {
 
+    services.ekklesia.portal.configFile = configInput;
+
     environment.systemPackages = [ ekklesiaPortalConfig ekklesiaPortalShowConfig ];
 
     users.users.ekklesia-portal = { };
@@ -76,18 +109,95 @@ in {
 
     systemd.services.ekklesia-portal = {
 
-      after = [ "network.target" ];
+      description = "Ekklesia E-Democracy Portal";
+      after = [ "network.target" "postgresql.service" ];
       wantedBy = [ "multi-user.target" ];
       stopIfChanged = false;
+
+      preStart = let
+        replaceDebug = lib.optionalString cfg.debug "-vv";
+        secrets = cfg.secretFiles // {
+          browser_session_secret_key = cfg.browserSessionSecretKeyFile;
+        };
+        replaceSecret = file: var: secretFile:
+        "${pkgs.replace}/bin/replace-literal -m 1 ${replaceDebug} -f -e @${var}@ $(< ${secretFile}) ${file}";
+        replaceCfgSecret = var: secretFile: replaceSecret "$cfgdir/${configFilename}" var secretFile;
+        secretReplacements = lib.mapAttrsToList (k: v: replaceCfgSecret k v) cfg.secretFiles;
+      in ''
+        echo "Prepare config file..."
+        cfgdir=$RUNTIME_DIRECTORY
+        chmod u+w -R $cfgdir
+        cp ${configInput} $cfgdir/${configFilename}
+
+        ${lib.concatStringsSep "\n" secretReplacements}
+
+        echo "Run database migrations if needed..."
+        ${serveApp}/bin/migrate
+        echo "Pre-start finished."
+      '';
 
       serviceConfig = {
         User = cfg.user;
         Group = cfg.group;
-        ExecStartPre = "${serveApp}/bin/migrate";
         ExecStart = "${serveApp}/bin/run";
-        X-ConfigFile = appConfigFile;
+        RuntimeDirectory = "ekklesia-portal";
+        StateDirectory = "ekklesia-portal";
+        RestartSec = "5s";
+        Restart = "always";
+        X-ConfigFile = configInput;
+        X-ServeApp = serveApp;
+
+        DeviceAllow = [
+          "/dev/stderr"
+          "/dev/stdout"
+        ];
+
+        AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+        CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+        DevicePolicy = "strict";
+        LockPersonality = true;
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateTmp = true;
+        PrivateUsers = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "strict";
+        RemoveIPC = true;
+        RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          # deny the following syscall groups
+          "~@clock"
+          "~@debug"
+          "~@module"
+          "~@mount"
+          "~@reboot"
+          "~@cpu-emulation"
+          "~@swap"
+          "~@obsolete"
+          "~@privileged"
+          "~@resources"
+          "~@raw-io"
+        ];
+        UMask = "077";
+
       };
 
+      unitConfig = {
+        Documentation = [
+          "https://github.com/edemocracy/ekklesia-portal"
+          "https://ekklesiademocracy.org"
+        ];
+      };
     };
 
   };
