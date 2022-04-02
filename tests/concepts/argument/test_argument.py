e44dcdd95106b3eb8ba0730052b49e12b554edf7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-02 12:46:24 +0800

    Delete tests directory

--- tests/concepts/argument/test_argument.py
+++
@@ -1,6 +0,0 @@
-def test_argument(client):
-    """XXX: depends on content from create_test_db.py"""
-    res = client.get("/arguments/3")
-    assert 'Ein Contra-Argument' in res  # title
-    assert 'dagegen' in res  # abstract
-    assert 'aus Gründen' in res  # details
--- tests/concepts/ekklesia_portal/test_logout.py
+++
@@ -1,8 +0,0 @@
-from tests.helpers.webtest_helpers import get_session
-
-
-def test_logout(app, client):
-    client.post("/login", dict(username="testuser", password="test"), status=302)
-    client.post('/logout', status=302)
-    session = get_session(app, client)
-    assert "user_id" not in session
