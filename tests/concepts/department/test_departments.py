49216000b0934c084ce51abc848c1203f875f1b3
ge95tic <ge95tic@tum.de>
2022-05-17 14:56:42 +0200

    Added tests. Done.

--- tests/concepts/department/test_departments.py
+++ tests/concepts/department/test_departments.py
@@ -37,10 +37,6 @@ def test_department_view_not_logged_in(client):
 
 def test_department_view_logged_in(client, logged_in_user_with_departments):
     res = client.get('/d')
-    if logged_in_user_with_departments.departments:
-        assert 'My departments' in res
-        for department in logged_in_user_with_departments.departments:
-            assert department.name in res
-
-
-
+    assert 'My departments' in res
+    for department in logged_in_user_with_departments.departments:
+        assert department.name in res
