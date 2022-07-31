71c60f3cb197928d93ad1e84acb1678f05e9b42a
Tobias dpausp <dpausp@posteo.de>
2022-07-31 00:24:37 +0000

    Fix unsafe repo check in git_version.nix
    Newer Git versions check if a dir is owned by the user executing
    the Git command. This is not the case if we run a Git command
    on the Nix store, so Git refuses to do its work. Add a config
    file for Git to fix this.
    
    Fixes static file and docker image building.

--- nix/git_version.nix
+++ nix/git_version.nix
@@ -4,14 +4,22 @@ with builtins;
 with pkgs;
 
 let
+  # Git complains about an "unsafe repository" without this global config.
+  gitConf = pkgs.writeText "gitconfig-safedir-hack" ''
+  [safe]
+      directory = ${../.}
+  '';
   gitVersion =
     lib.replaceStrings
       ["\n"]
       [""]
       (lib.readFile
         (runCommand
-          "git-version"
-          { src = ../.; buildInputs = [ gitMinimal ]; }
+          "git-version" {
+            src = ../.;
+            buildInputs = [ gitMinimal ];
+            GIT_CONFIG_GLOBAL = gitConf;
+          }
           "cd $src; git describe --long --tags --dirty --always > $out"));
 in
   if pathExists ../.git
