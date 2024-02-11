f45310a927751e4ba55e63be9766f3f8388ae28d
Nico Borgsmüller <me@kaenganxt.dev>
2024-02-11 23:14:59 +0100

    Hacky fix for when Discourse doesn't accept our tags

--- src/ekklesia_portal/lib/discourse.py
+++ src/ekklesia_portal/lib/discourse.py
@@ -25,19 +25,26 @@ class DiscourseError(Exception):
     pass
 
 
-def create_discourse_topic(config: DiscourseConfig, topic: DiscourseTopic):
+def create_discourse_topic(config: DiscourseConfig, topic: DiscourseTopic, without_tags: bool = False):
 
     headers = {'accept': 'application/json', 'api-key': config.api_key, 'api-username': config.api_username}
 
-    req = {'raw': topic.content, 'category': config.category, 'title': topic.title, 'tags': topic.tags}
+    if without_tags:
+        req = {'raw': topic.content, 'category': config.category, 'title': topic.title}
+    else:
+        req = {'raw': topic.content, 'category': config.category, 'title': topic.title, 'tags': topic.tags}
 
-    with start_action(action_type="discourse_post") as action:
+    with start_action(action_type="discourse_post", without_tags=without_tags) as action:
         resp = requests.post(f"{config.base_url}/posts.json", json=req, headers=headers)
         action.add_success_fields(response=resp.json())
 
     try:
         resp.raise_for_status()
     except HTTPError as e:
-        raise DiscourseError(e)
+        if e.response.status_code == 422 and not without_tags:
+            # Try again once without tags
+            return create_discourse_topic(config, topic, True)
+        else:
+            raise DiscourseError(e)
 
     return resp
